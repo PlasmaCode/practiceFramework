3a9bd071e67de48349f8e8013cbba91dd4189837
PlasmaCode <noxusblade@hotmail.com>
2015-12-07 01:32:37 -0500

    Updated

--- Framework/AutoLoader.php
+++ Framework/AutoLoader.php
@@ -7,7 +7,10 @@ class AutoLoader
     }
     public function controllerLoader($controller)
     {
-        $path = __DIR__ . '/../App/Controllers';
-        include $path . '/' . $controller . '.php';
+        $path = __DIR__ . '/../App/Controllers/';
+        $file = $path . $controller . '.php';
+        if(file_exists($file)) {
+            include $file;
+        }
     }
 }
